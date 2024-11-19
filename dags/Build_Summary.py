3e5cbc802eab90c84c8379208b32c990ce431186
giung <sko99@naver.com>
2024-11-19 20:07:14 +0900

    schema sko99로 변경

--- dags/Build_Summary.py
+++ dags/Build_Summary.py
@@ -61,7 +61,7 @@ execsql = PythonOperator(
     task_id = 'mau_summary',
     python_callable = execSQL,
     params = {
-        'schema' : 'keeyong',
+        'schema' : 'sko99',
         'table': 'mau_summary',
         'sql' : """SELECT 
   TO_CHAR(A.ts, 'YYYY-MM') AS month,
